57f06472cc2656f3dc3e3bfd2f60238dfee1374d
junaid1506 <junnu2757@gmail.com>
2026-01-28 13:16:08 +0530

    Update

--- 01.HTTP/server.js
+++ 01.HTTP/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+
 const app = express();
 const NodeCache = require("node-cache");
 const productCache = new NodeCache({ stdTTL: 10 }); // Cache with a TTL of 60 seconds
+const redis = require("redis");
+redisClient = redis.createClient();
+// redisClient.on("error", (err) => console.log("Redis Client Error", err));
+redisClient.connect();
 
 app.get("/", (req, res) => {
   res.header("Content-Type", "text/plain");
@@ -25,6 +30,23 @@ app.get("/products", (req, res) => {
   res.json({
     source: "db",
     data: products,
+  });
+});
+
+app.get("/users", async (req, res) => {
+  const cachedUsers = await redisClient.get("users");
+  if (cachedUsers) {
+    return res.json({ source: "redis cache", data: JSON.parse(cachedUsers) });
+  }
+  const users = [
+    { id: 1, name: "User A" },
+    { id: 2, name: "User B" },
+    { id: 3, name: "User C" },
+  ];
+  await redisClient.setEx("users", 30, JSON.stringify(users)); // Cache for 60 seconds
+  res.json({
+    source: "db",
+    data: users,
   });
 });
 
